b8ebd5883809133847d8e3fe30dea447588b6d4d
關本剛史 <sekimoto_tsuyoshi@ip-10-231-236-150.ap-northeast-1.compute.internal>
2025-04-22 16:51:24 +0900

    cssも読み込めるように

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,7 +1,11 @@
 // src/global.d.ts
 
+// CSS モジュール用
 declare module '*.module.css' {
     const classes: { [key: string]: string };
     export default classes;
   }
+  
+  // グローバル CSS 用 
+  declare module '*.css';
   
