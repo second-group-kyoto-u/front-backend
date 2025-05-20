bb06ed7c89a4a256bac74d31b8d35cb2f19e4cb1
Ena <ena.ohbayashi.815@gmail.com>
2025-05-20 18:13:45 +0900

    がんばれ、、、

--- frontend/src/pages/TalkList/index.tsx
+++ frontend/src/pages/TalkList/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getJoinedEvents } from '@/api/event'
 import { getDirectMessageOverview } from '@/api/friend'
-import './TalkListPage.css'
+import './talklist.module.css'
 
 interface Event {
   id: string
